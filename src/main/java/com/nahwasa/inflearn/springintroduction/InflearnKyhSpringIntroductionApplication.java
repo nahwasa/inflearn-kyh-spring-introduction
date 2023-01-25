c398f21deaeb1e2b6fb162d1e55dd1d24e2a93b8
nahwasa <nahwasa@gmail.com>
2023-01-25 21:30:15 +0900

    init

---
+++ src/main/java/com/nahwasa/inflearn/springintroduction/InflearnKyhSpringIntroductionApplication.java
@@ -0,0 +1,13 @@
+package com.nahwasa.inflearn.springintroduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnKyhSpringIntroductionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnKyhSpringIntroductionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nahwasa/inflearn/springintroduction/InflearnKyhSpringIntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nahwasa.inflearn.springintroduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnKyhSpringIntroductionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
