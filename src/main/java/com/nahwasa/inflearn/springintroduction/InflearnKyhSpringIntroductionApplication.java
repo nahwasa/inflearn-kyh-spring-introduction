fdc6c60b089b04a6345db37f67ca24062fbde21d
nahwasa <nahwasa@gmail.com>
2023-01-26 22:42:40 +0900

    [H2 데이터베이스 설치] 로컬에 H2 DB 설치하기 귀찮아서 강의코드 마개조

--- src/main/java/com/nahwasa/inflearn/springintroduction/InflearnKyhSpringIntroductionApplication.java
+++ src/main/java/com/nahwasa/inflearn/springintroduction/InflearnKyhSpringIntroductionApplication.java
@@ -1,10 +1,27 @@
 package com.nahwasa.inflearn.springintroduction;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 public class InflearnKyhSpringIntroductionApplication {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public InflearnKyhSpringIntroductionApplication(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @PostConstruct
+    void checkTable() {
+        System.out.println(jdbcTemplate.queryForList("SELECT * FROM MEMBER").get(0));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(InflearnKyhSpringIntroductionApplication.class, args);
