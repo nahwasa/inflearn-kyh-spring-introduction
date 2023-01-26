9fc8ee571e7608638ade203e930b2a1eded89e4c
nahwasa <nahwasa@gmail.com>
2023-01-26 20:55:52 +0900

    [회원 서비스 테스트] 일단 테스트만 통과하도록 함. 다만 memberRepository가 중복되는 문제가 있음.

--- src/test/java/com/nahwasa/inflearn/springintroduction/service/MemberServiceTest.java
+++ src/test/java/com/nahwasa/inflearn/springintroduction/service/MemberServiceTest.java
@@ -1,7 +1,10 @@
 package com.nahwasa.inflearn.springintroduction.service;
 
 import com.nahwasa.inflearn.springintroduction.domain.Member;
+import com.nahwasa.inflearn.springintroduction.repository.MemberRepository;
+import com.nahwasa.inflearn.springintroduction.repository.MemoryMemberRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,6 +12,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class MemberServiceTest {
 
     MemberService memberService = new MemberService();
+    MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+        // 테스트끼리 영향을 끼치지 않게 하기위한 clear 호출을 위해 가져옴. 근데 MemberService에 있는거랑 다른 객체임(new 해서).
+        // static으로 들어가있었어서 문제는 없었지만, 일반적으로 안좋은 구조임.
+
+    @AfterEach
+    public void afterEach() {   // 테스트끼리 영향을 끼치지 않도록.
+        memberRepository.clearStore();
+    }
 
     @Test
     void 회원가입() {   // 테스트는 영어권 사람들이랑 일하는게 아니라면 한글로 적어도 상관없다고 함.
