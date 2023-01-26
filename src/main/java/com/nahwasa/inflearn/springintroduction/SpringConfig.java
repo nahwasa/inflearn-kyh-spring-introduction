45f6262a77ed0272160b007310005915d927f8c5
nahwasa <nahwasa@gmail.com>
2023-01-26 21:21:16 +0900

    [자바 코드로 직접 스프링 빈 등록하기]

---
+++ src/main/java/com/nahwasa/inflearn/springintroduction/SpringConfig.java
@@ -0,0 +1,29 @@
+package com.nahwasa.inflearn.springintroduction;
+
+import com.nahwasa.inflearn.springintroduction.repository.MemberRepository;
+import com.nahwasa.inflearn.springintroduction.repository.MemoryMemberRepository;
+import com.nahwasa.inflearn.springintroduction.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    /**
+     * @Service랑 @Repository 쓰지 않고 직접 스프링 컨테이너에 등록하는 방법임.
+     * @Controller는 이런식으로 대체 안됨.
+     * 일반적으로 그냥 어노테이션 써서 하는게 편함.
+     * 실무에서는 주로 정형화된 컨트롤러, 서비스, 리포지토리 같은 코드는 컴포넌트 스캔을 사용한다.
+     * 그리고 정형화되지 않거나, 상황에 따라 구현 클래스를 변경해야 하면 설정을 통해 스프링 빈으로 등록한다.
+     * (예를들어 현재 MemoryMemberRepository()를 쓰고 있는데, 실제 DB 연결해서 바꿔치기할 때 기존 코드 변경 없이 아래쪽 설정만 바꾸면 끝)
+     */
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
