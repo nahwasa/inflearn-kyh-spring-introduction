a045da45605c80da5eda0ec42595a0b78e278364
nahwasa <nahwasa@gmail.com>
2023-01-26 22:02:56 +0900

    [회원 웹 기능 - 등록]

--- src/main/java/com/nahwasa/inflearn/springintroduction/controller/MemberController.java
+++ src/main/java/com/nahwasa/inflearn/springintroduction/controller/MemberController.java
@@ -1,8 +1,11 @@
 package com.nahwasa.inflearn.springintroduction.controller;
 
+import com.nahwasa.inflearn.springintroduction.domain.Member;
 import com.nahwasa.inflearn.springintroduction.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -14,6 +17,19 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
 
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
 
 }
